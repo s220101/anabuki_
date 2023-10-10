317ec5538ddae37c5dd57ca443604d91cd341b81
穴吹　琉賢 <126172383+s220101@users.noreply.github.com>
2023-10-10 16:54:18 +0900

    Add files via upload
    This folder is where the Arduino language that sends messages to LineNotify using ESP32 is stored. Messages are not only text, but also numerical data can be sent.

---
+++ Arduino言語/Line_Notify/ESP32_Line_Notify_Hello_LineNotify.ino
@@ -0,0 +1,38 @@
+#include <WiFi.h>
+#include <ESP_Line_Notify.h>
+
+// WiFi 設定
+const char *WIFI_SSID = "";        //  *** 書き換え必要 ***
+const char *WIFI_PASSWORD = "";  //  *** 書き換え必要（8文字以上）***
+
+LineNotifyClient line;
+WiFiServer server(80);
+ 
+void setup()
+{
+  Serial.println("Setup start");
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  { 
+    delay(200);
+    Serial.println(".");
+  }
+  Serial.println(WiFi.localIP());
+  server.begin();
+  Serial.println("OK!");
+
+// LINEトークンの入力
+  line.token = "";
+  Serial.println("Setup END");
+}
+ 
+ 
+void loop()
+{
+  line.message = "Hello Line Notify!!";
+  LineNotifySendingResult result = LineNotify.send(line);
+  delay(5000);
+  Serial.println("loop end");
+
+}
