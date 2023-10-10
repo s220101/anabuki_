bb12e55938d86c34c917fe74cfe6ea10340bddbd
穴吹　琉賢 <126172383+s220101@users.noreply.github.com>
2023-10-10 17:25:33 +0900

    Add files via upload

--- Arduino言語/Google_sheets/esp32_google_sheet_BME280.ino
+++ Arduino言語/Google_sheets/esp32_google_sheet_BME280.ino
@@ -1,4 +1,7 @@
 //https://lastminuteengineers.com/bme280-arduino-tutorial/
+#include <SPI.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
@@ -6,11 +9,34 @@
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BME280 bme;
+/* WiFiのssidとパスワードを記入 */
+const char* ssid = "";
+const char* pass = "";
+
+WiFiServer server(80);
+
+const String host = "";
 
 void setup() {
   Serial.begin(115200);
 
-  if (!bme.begin(0x76)) {
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  WiFi.begin(ssid, pass);
+  
+  /* Wi-Fi接続確認 */
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("connected");
+
+  server.begin();
+  Serial.print("server: ");
+  Serial.println(WiFi.localIP());
+
+  if (!bme.begin(0x76)) 
+  {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
     while (1);
   }
